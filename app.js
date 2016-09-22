bb098f5425d2be1461458989f19000274dcc9118
Kevin Huelsmann <kjh311@gmail.com>
2016-09-22 14:04:44 -0700

    update

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-$(document).ready(function(){});
+$(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() > $(document).height() - 1290) {
+       // alert("Bottom Reached!");
+       // $('.scrollable-links').addClass('scrollable-links-fixed');
+       // $('.white-article-main').addClass('align-right pull-right offset-md-4');
+   }else{
+    // $('.scrollable-links').removeClass('scrollable-links-fixed');
+   }
+});
