2301195494e52719c6753f33b1c730fcb60a01ea
Kevin Huelsmann <kjh311@gmail.com>
2016-09-23 08:12:29 -0700

    carousel added

--- app.js
+++ app.js
@@ -7,3 +7,42 @@ $(window).scroll(function() {
     // $('.scrollable-links').removeClass('scrollable-links-fixed');
    }
 });
+
+
+
+// var $carousel = $('#myCarousel');
+// $carousel.carousel();
+
+// $carousel.bind('slide.bs.carousel', function (e) {
+//     alert('slide event!');
+// });
+
+// $('#myCarousel').bind('slide.bs.carousel', function (e) {
+//     console.log('slide event!');
+// });
+
+// $('#myCarousel').bind('slid', function (e) {
+//     console.log("slid event!");
+// });
+
+
+$(function() {
+
+  // Start slide animation
+  $("#myCarousel").on('slide.bs.carousel', function(event){
+    console.log('slide');
+
+    // var active = $(event.target).find('.carousel-inner > .item.active');
+    // var from = active.index();
+    // var next = $(event.relatedTarget);
+    // var to = next.index();
+    // var direction = event.direction;
+
+    // console.log('prev', active.prevOrLast());
+    // console.log('active', active, from);
+    // console.log('next', next, to);
+    // console.log('direction', direction);
+      alert('sdf');
+
+  });
+  });
